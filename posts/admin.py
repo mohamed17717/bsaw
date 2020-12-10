c38bc78448e12a9af7402136fd1726cc18f0df83
Mo17 <mo.err17@gmail.com>
2020-12-10 06:39:51 +0200

    stop delete admin

--- posts/admin.py
+++ posts/admin.py
@@ -6,6 +6,8 @@ admin.site.register(Author)
 admin.site.register(Category)
 admin.site.register(Tag) 
 
+admin.site.disable_action('delete_selected')
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
   exclude = ('creator', 'author', 'seen_count')
